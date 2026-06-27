0ffd4d8114b6040f47eb3c02ddaf87395d2300ca
Prathita Kumar Madhusudhana <prathita2003@gmail.com>
2026-06-27 13:19:48 +0530

    Updated Security config

--- src/main/java/com/orchestrator/workflow/security/SecurityConfig.java
+++ src/main/java/com/orchestrator/workflow/security/SecurityConfig.java
@@ -1,35 +1,77 @@
 package com.orchestrator.workflow.security;
 
-import org.springframework.context.annotation.*;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class SecurityConfig 
-{
+public class SecurityConfig {
+
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
-        http.csrf(csrf->csrf.disable())
-                .authorizeHttpRequests(auth-> auth
-                .requestMatchers("/workflows/**")
-                .hasRole("ADMIN")
-                .requestMatchers("/executions/start/**")
-                .hasAnyRole("ADMIN","OPERATOR")
-                .requestMatchers("/executions/*/tasks/*/complete","/executions/*/tasks/*fail","/executions/*/tasks/*/start")
-                        .hasAnyRole("ADMIN","OPERATOR")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/workflows/**")
+                            .hasRole("ADMIN")
+
+                        .requestMatchers("/executions/start/**")
+                            .hasAnyRole("ADMIN", "OPERATOR")
+
+                        .requestMatchers(
+                                "/executions/*/tasks/*/complete",
+                                "/executions/*/tasks/*/fail",
+                                "/executions/*/tasks/*/start")
+                            .hasAnyRole("ADMIN", "OPERATOR")
+
                         .anyRequest()
-                        .authenticated())
+                            .authenticated()
+                )
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
