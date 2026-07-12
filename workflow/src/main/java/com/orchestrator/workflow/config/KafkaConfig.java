eae59d07f88d7fe15273c3650143850476687959
Prathita Kumar Madhusudhana <prathita2003@gmail.com>
2026-07-12 18:36:53 +0530

    Updated Workflow Orchestrator v1

--- workflow/src/main/java/com/orchestrator/workflow/config/KafkaConfig.java
+++ workflow/src/main/java/com/orchestrator/workflow/config/KafkaConfig.java
@@ -1,24 +1,18 @@
 package com.orchestrator.workflow.config;
-
+//Can be used only with locally available kafka
 import com.orchestrator.workflow.dto.WorkflowEvent;
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
 import java.util.*;
 
 @EnableKafka
